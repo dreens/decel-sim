4953780c2f54af19450a7e34549a304b8e3fd932
Anna McAuliffe <anmc7126@colorado.edu>
2018-03-01 12:31:20 -0700

    Collision Hunt Figure w/ Tof

--- resultsdecel.m
+++ resultsdecel.m
@@ -52,12 +52,35 @@ function resultsdecel(rs)
     set(gca,'FontSize',12)
     grid on
 
-    subplot(2,3,5)
-    xlabel('X Position (mm)','FontSize',12)
-    ylabel('X Velocity (m/s)','FontSize',12)
-    title('Phase Space X','FontSize',14)
-    set(gca,'FontSize',12)
+    
+    
+    
+    subplot (2,3,5)
+    colors = get(gca,'ColorOrder');
+    colors = [colors ; colors ; colors ; colors];
+    diamL = 2.5e-3;
+    distL = 8e-3 + r.pos(1,3);
+    for j=1:length(rs)
+        r = rs(j);
+        tof = zeros(1,1000);
+        times = 1e-7:1e-7:1e-4;
+        for i=1:1000
+            t = times(i);
+            zsq = (r.pos(:,3)-distL + t*r.vel(:,3)).^2;
+            xsq = (r.pos(:,1)+t*r.vel(:,1)).^2;
+            ya = abs(r.pos(:,2)+t*r.vel(:,2));
+            tof(i) = sum( zsq + xsq < diamL^2/4 & ya < 2e-3);
+        end
+        plot(times*1e6,tof,'Color',colors(j,:))
+    end
+    xlabel('Time \mus')
+    ylabel('Population (arb)')
+    title('Time of Flight')
+    legend('11 mm','22 mm','33 mm')
     grid on
+    
+    
+    
     
     subplot(2,3,6)
     xlabel('Initial Deceleration Stages','FontSize',12)

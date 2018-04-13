2762e70de070bc20b5656a7aa4aa8ba636646c8f
David Reens <dave.reens@gmail.com>
2018-04-13 00:13:45 -0600

    Added commentary to study2field. Modified entry so it can be used to study ppmm only type sequences.

--- studyfield_compilation.m
+++ studyfield_compilation.m
@@ -1,11 +1,11 @@
-v = VideoWriter('synch_trap_pggg.gif');
+v = VideoWriter('synch_trap_ppmmonly');
 v.FrameRate = 1;
 open(v);
-for i=0:10:90
-    h = study2fields('longdecel','singlerod',i);
+for p=0:5:90
+    h = study2fields('longdecel','ppmm_2mm',-135+p/2,-45+p/2,p);
     drawnow
     %h = studyfield('longdecel',i);
     writeVideo(v,getframe(h));
 end
 close(v)
-
+close all

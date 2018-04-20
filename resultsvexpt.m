3ce7e4b3b3119d3119e821c73bf5cf844dab62d6
David Reens <dave.reens@gmail.com>
2018-04-19 23:21:46 -0600

    Got first decent version of sim-exp match for paper

--- resultsvexpt.m
+++ resultsvexpt.m
@@ -1,7 +1,7 @@
 function resultsvexpt(rsf,data)
 
 figure; hold on
-scale = 1/60;
+scale = 1/50;
 colors = [...
     0.2081    0.1663    0.5292;
     0.0582    0.4677    0.8589;
@@ -12,7 +12,9 @@ colors = [...
 speeds = [800 500 200 100 50 37];
 for i=1:length(rsf)
     r = rsf(i);
-    diamL = 2e-3; 
+    r.vel = r.vel(abs(r.pos(:,3)-r.pos(1,3))<5e-3,:);
+    r.pos = r.pos(abs(r.pos(:,3)-r.pos(1,3))<5e-3,:);
+    diamL = 1.8e-3; 
     distL = 10e-3 + r.pos(1,3) - r.phase*5e-3/180;
     tof = zeros(1,3500);
     times = (1e-7)*(1:length(tof));
@@ -28,8 +30,11 @@ for i=1:length(rsf)
         l = '-';
     else
         l = '--';
+        tof = -tof;
     end
-    shift = -(r.finalvz==800)*20;
+    shift = -(r.finalvz==800)*20 + (r.finalvz==37)*70;
+    times = times(tof~=0);
+    tof = tof(tof~=0);
     plot(times*1e6+shift,tof*scale,'Color',c,'LineStyle',l)
 end
 
@@ -40,8 +45,9 @@ for i=1:length(data)
         m = '.'; ms = 18;
     else
         m = 'o'; ms = 6;
+        d.YData = -d.YData;
     end
-    shift = -(d.speed==800)*20;
+    shift = -(d.speed==800)*20 + (d.speed==37)*70;
     errorbar(d.XData+shift,d.YData,d.UData,'LineStyle','none','Color',c,'Marker',m,'MarkerSize',ms)
 end
 

cc2e25d22b856ffc1355544ace4624335963d2f8
David Reens <dave.reens@gmail.com>
2018-04-26 16:31:57 -0600

    Added Figures for VVSF2

--- makeArray.m
+++ makeArray.m
@@ -3,7 +3,8 @@ out = efftrap3D('longdecel','longdecel',phi-180,-phi,phi);
 outpggg = efftrap3D('longdecel','singlerod',phi-180,-phi,phi);
 outppgg = efftrap3D('longdecel','ppgg',phi-180,-phi,phi);
 outppmm = efftrap3D('longdecel','ppmm_2mm',phi-180,-phi,phi);
-outpmpm = efftrap3D('pmpm_2mm_no-sym','ppgg',-109,-19,71);
+%outpmpm = efftrap3D('pmpm_2mm_no-sym','ppgg',-109,-19,71);
+outppmm2 = efftrap3D('pmpm_2mm_no-sym','ppmm_2mm',-120,-20,60);
 %plotTrapSlice({outppgg,outpggg,out},0,'contourArray')
 
 %%
@@ -15,4 +16,4 @@ plotTrapSlice({outppmm,outppgg,outpggg,out},39,'contourArray');
 
 %% try generalized efftrap
 outmod = efftrap3Dgen({'ppgg','longdecel','ppgg','pmpm_2mm_no-sym'},[-132 -50 68 160 228],[1 1 0 0]);
-plotTrapSlice({outmod, outpmpm, outppgg,outpggg},39,'contourArray');
+plotTrapSlice({outppmm2, outppmm, outppgg,outpggg},39,'contourArray');

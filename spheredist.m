179346b78678569288b7ef264b5f219a1632a6c8
David Reens <dave.reens@gmail.com>
2018-05-17 17:07:30 -0600

    Started Writing a spherical distribution generator.

---
+++ spheredist.m
@@ -0,0 +1,16 @@
+%% Spherical Distribution Function
+% This function returns an array of points that are uniformly distributed
+% within the volume of a sphere of dimension dim and unit radius. 
+function points = spheredist(N,dim,varargin)
+
+
+
+
+
+
+
+
+
+
+
+end

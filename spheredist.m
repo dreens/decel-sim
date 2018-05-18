5d2a53ea775c44bfcd9600dfdf6a50e7b7e3e042
David Reens <dave.reens@gmail.com>
2018-05-18 08:16:51 -0600

    A few more lines in spheredist

--- spheredist.m
+++ spheredist.m
@@ -2,8 +2,9 @@
 % This function returns an array of points that are uniformly distributed
 % within the volume of a sphere of dimension dim and unit radius. 
 function points = spheredist(N,dim,varargin)
-
-
+r = (rand(N,1))^(1/dim);
+x = 0:1e-3:1;
+p = sin(x);
 
 
 

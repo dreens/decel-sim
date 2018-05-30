9b6944229070d6662d653b23b2c23422a905df68
David Reens <dave.reens@gmail.com>
2018-05-29 21:03:50 -0600

    Made array for S=3 delay switching. Not too much help.

--- makeArray.m
+++ makeArray.m
@@ -10,9 +10,14 @@ outppmm2 = efftrap3D('pmpm_2mm_no-sym','ppmm_2mm',-112.5,-20,67.5);
 %%
 
 outs3 = efftrap3D('longdecel','longdecel',phi-540,-phi,phi);
+%see if delay switching im S=3 matters. It doesnt. 10% effect maybe. 
+outs3p = efftrap3D('longdecel','singlerod',phi-540,-360-phi,phi);
+
 %plotTrapSlice(outs3,39,'yz')
 %%
 plotTrapSlice({outppmm,outppgg,outpggg,out},39,'contourArray');
+plotTrapSlice({outs3p,outs3,outpggg,out},39,'contourArray');
+
 
 %% try generalized efftrap
 outmod = efftrap3Dgen({'ppgg','longdecel','ppgg','pmpm_2mm_no-sym'},[-132 -50 68 160 228],[1 1 0 0]);

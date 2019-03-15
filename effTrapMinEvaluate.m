f5772f0a6c33828134aa9a5f4806581f3852e491
David Reens <dave.reens@gmail.com>
2019-03-15 10:33:07 -0600

    Got min depth v phase working. Decided not to mess with definition of SF, VSF, XSF.

--- effTrapMinEvaluate.m
+++ effTrapMinEvaluate.m
@@ -1,36 +1,38 @@
 %%
 % Make use of the Min Depth Finder to study some plots!
 %
-phis = 5:5:80;
-if ~exist('out')
+phis = [89];%5:5:85];
+if false%~exist('out')
     out = {};
     outpggg = {};
     outppgg = {};
     outppmm = {};
+    s1 = [];
+    sf = [];
+    vsf = [];
+    xsf = [];
 end
-s1 = [];
-sf = [];
-vsf = [];
-xsf = [];
 for phi=phis
     phi
-
-    out{phi} = efftrap3Dgen({'longdecel','longdecel'},[phi-180,-phi,phi],[1 1]);
-    outpggg{phi} = efftrap3Dgen({'singlerod','longdecel'},[phi-180,-phi,phi],[1 1]);
-    outppgg{phi} = efftrap3Dgen({'ppgg','longdecel'},[phi-180,-phi,phi],[1 1]);
-    outppmm{phi} = efftrap3Dgen({'ppmm_2mm','longdecel'},[phi-180,-phi,phi],[1 1]);
-
-    s1 = [s1 effTrapMinDepth(out{phi})];
-    sf = [sf effTrapMinDepth(outpggg{phi})];
-    vsf = [vsf effTrapMinDepth(outppgg{phi})];
-    xsf = [xsf effTrapMinDepth(outppmm{phi})];
+    midphi = -phi;
+    if phi>length(out) || isempty(out{phi})
+        out{phi} = efftrap3Dgen({'longdecel','longdecel'},[phi-180,midphi,phi],[1 1]);
+        outpggg{phi} = efftrap3Dgen({'singlerod','longdecel'},[phi-180,midphi,phi],[1 1]);
+        outppgg{phi} = efftrap3Dgen({'ppgg','longdecel'},[phi-180,midphi,phi],[1 1]);
+        outppmm{phi} = efftrap3Dgen({'ppmm_2mm','longdecel'},[phi-180,midphi,phi],[1 1]);
+    end
+    s1(phi)  = effTrapMinDepth(out{phi});
+    sf(phi)  = effTrapMinDepth(outpggg{phi});
+    vsf(phi) = effTrapMinDepth(outppgg{phi});
+    xsf(phi) = effTrapMinDepth(outppmm{phi});
 end
 
 figure;
-plot(phis,s1,'DisplayName','S=1')
+phis = [1, 5:5:85, 89];
+plot(phis,s1(phis),'DisplayName','S=1')
 hold on
-plot(phis,sf,'DisplayName','SF')
-plot(phis,vsf,'DisplayName','VSF')
-plot(phis,xsf,'DisplayName','XSF')
+plot(phis,sf(phis),'DisplayName','SF')
+plot(phis,vsf(phis),'DisplayName','VSF')
+plot(phis,xsf(phis),'DisplayName','XSF')
 legend('show')
     

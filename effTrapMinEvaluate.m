1e4391ac1bae12017867c73505af0aa9f1abf2ee
David Reens <dave.reens@gmail.com>
2019-03-15 09:03:25 -0600

    Corrected Existence Check on evaluation code.

--- effTrapMinEvaluate.m
+++ effTrapMinEvaluate.m
@@ -2,18 +2,24 @@
 % Make use of the Min Depth Finder to study some plots!
 %
 phis = 5:5:80;
+if ~exist('out')
+    out = {};
+    outpggg = {};
+    outppgg = {};
+    outppmm = {};
+end
 s1 = [];
 sf = [];
 vsf = [];
 xsf = [];
 for phi=phis
     phi
-    if ~exist('out')
-        out{phi} = efftrap3Dgen({'longdecel','longdecel'},[phi-180,-phi,phi],[1 1]);
-        outpggg{phi} = efftrap3Dgen({'singlerod','longdecel'},[phi-180,-phi,phi],[1 1]);
-        outppgg{phi} = efftrap3Dgen({'ppgg','longdecel'},[phi-180,-phi,phi],[1 1]);
-        outppmm{phi} = efftrap3Dgen({'ppmm_2mm','longdecel'},[phi-180,-phi,phi],[1 1]);
-    end
+
+    out{phi} = efftrap3Dgen({'longdecel','longdecel'},[phi-180,-phi,phi],[1 1]);
+    outpggg{phi} = efftrap3Dgen({'singlerod','longdecel'},[phi-180,-phi,phi],[1 1]);
+    outppgg{phi} = efftrap3Dgen({'ppgg','longdecel'},[phi-180,-phi,phi],[1 1]);
+    outppmm{phi} = efftrap3Dgen({'ppmm_2mm','longdecel'},[phi-180,-phi,phi],[1 1]);
+
     s1 = [s1 effTrapMinDepth(out{phi})];
     sf = [sf effTrapMinDepth(outpggg{phi})];
     vsf = [vsf effTrapMinDepth(outppgg{phi})];

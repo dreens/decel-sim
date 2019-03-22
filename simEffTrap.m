3676393355cb8c7342527c27bd972b5e2aea6971
David Reens <dave.reens@gmail.com>
2019-03-22 14:10:50 -0600

    Caught a bug in the Phase Space Volume which results in it being over-counted by a factor of nine. The z-size adjustment was under the cube instead of outside.

--- simEffTrap.m
+++ simEffTrap.m
@@ -74,7 +74,7 @@ function varargout = simEffTrap(varargin)
     
     num = size(pos,1);
     temp = mean(.5*mOH*sum(vel.^2,2))/1.38e-23;
-    initPSV = (2*settings.maxVel)^3 * (2e-3 * settings.zwidth)^3;
+    initPSV = (2*settings.maxVel)^3 * (2e-3)^3  * settings.zwidth;
     psv = num/settings.num * initPSV;
     varargout = {num,psv,temp};
     varargout = varargout(1:max(nargout,1));
